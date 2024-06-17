8974430cfabee225222f905fcbfc3ea4e9389346
ShawnEdgell <milky.irl@gmail.com>
2024-06-17 14:04:36 -0400

    removed nav tag

--- src/components/ui/navbar/navbar.tsx
+++ src/components/ui/navbar/navbar.tsx
@@ -3,24 +3,22 @@ import { Link } from "@builder.io/qwik-city";
 
 export const Navbar = component$(() => {
   return (
-    <nav class="fixed top-0 w-full bg-gray-800 p-4 text-white">
-      <ul class="flex space-x-4">
-        <li>
-          <Link href="/" class="hover:underline">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/about" class="hover:underline">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact" class="hover:underline">
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <ul class="flex space-x-4">
+      <li>
+        <Link href="/" class="hover:underline">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link href="/about" class="hover:underline">
+          About
+        </Link>
+      </li>
+      <li>
+        <Link href="/contact" class="hover:underline">
+          Contact
+        </Link>
+      </li>
+    </ul>
   );
 });
